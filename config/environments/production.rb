bc6ef8b6b827d4e9b451eb9b764de65d23403e2b
ocowchun <ocowchun@gmail.com>
2014-12-03 17:05:10 +0800

    comment production log formatter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,8 @@ Rails.application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  #comment this line for fluentd
+  # config.log_formatter = ::Logger::Formatter.new 
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
